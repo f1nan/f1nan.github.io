0f83b6d3d62c99a0a4fe2ff9419008bf2d52af03
= <23575239+f1nan@users.noreply.github.com>
2021-01-15 21:47:58 +0100

    Neuer Service-Worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,36 @@
-const cacheName = "v1.1";
+const cacheName = "v1.2";
+
+const cacheAssets = [
+    "/",
+    "/css/style.css",
+    "/images/icons/icon-48x48.png",
+    "/images/icons/icon-72x72.png",
+    "/images/icons/icon-96x96.png",
+    "/images/icons/icon-144x144.png",
+    "/images/icons/icon-152x152.png",
+    "/images/icons/icon-168x168.png",
+    "/images/icons/icon-192x192.png",
+    "/images/icons/icon-256x256.png",
+    "/images/icons/icon-512x512.png",
+    "/js/app.js",
+    "/js/create-todo.js",
+    "/js/elements.js",
+    "/js/todo-list-repository.js",
+    "/js/todo-list.js",
+    "/js/todo-storage.js",
+    "/js/view.js",
+    "/manifest.json",
+];
 
 self.addEventListener("install", (e) => {
     console.log("Service Worker: Installed");
+
+    e.waitUntil(
+        caches
+            .open()
+            .then((cache) => cache.addAll(cacheAssets))
+            .then(() => self.skipWaiting())
+    );
 });
 
 self.addEventListener("activate", (e) => {
@@ -24,12 +53,6 @@ self.addEventListener("fetch", (e) => {
     console.log("Service Worker: Fetching");
 
     e.respondWith(
-        fetch(e.request)
-            .then((res) => {
-                const resClone = res.clone();
-                caches.open(cacheName).then((cache) => cache.put(e.request, resClone));
-                return res;
-            })
-            .catch((err) => caches.match(e.request).then((res) => res))
+        fetch(e.request).catch(() => caches.match(e.request).then((res) => res))
     );
 });
